4f720aa1c5b909b78f28a8c8a5fa9ff4cbf40731
Eugene Sobol <esobol@insoftgroup.com>
2020-04-08 19:44:11 +0300

    Initial Commit

---
+++ TemperatureSensorMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TemperatureSensorMac
+//
+//  Created by Евгений Соболь on 4/8/20.
+//  Copyright © 2020 Esobol. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TemperatureSensorMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TemperatureSensorMac
+//
+//  Created by Евгений Соболь on 4/8/20.
+//  Copyright © 2020 Esobol. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
